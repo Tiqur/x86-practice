139f6540e0782a5866fc2877a978842890bd2db6
Tiqur <Tiqur7@gmail.com>
2021-10-03 01:06:06 -0700

    Read data from file

--- readfile/readfile.asm
+++ readfile/readfile.asm
@@ -5,19 +5,38 @@
   syscall
 %endmacro
 
+; Print string
+%macro print 1              ; define macro print with args (*char)
+  mov rax, %1               ; set rax to pointer to string
+  push rax                  ; push pointer of string to stack ( Will need it later )
+  mov rbx, 0                ; initialize index with 0 ( This will be used to count the total length of the string )
+
+  %%loop:
+    inc rax                 ; increment rax by 1 every loop
+    inc rbx                 ; increment rbx by 1 every loop
+    mov cl, [rax]           ; move the value of rax into register cl ( 8 bit )
+    cmp cl, 0               ; compare cl register to 0 ( end of string )
+    jne %%loop              ; loop if cl is not 0
+
+  mov rax, 1                ; sys_write
+  mov rdi, 1                ; set file descriptor
+  pop rsi                   ; pop location of string to rsi register
+  mov rdx, rbx              ; move length of string ( obtained through looping ) into rdx
+  syscall                   ; call kernel
+%endmacro
 
 section .data
-  filename db "data.txt", 0     ; 0 terminated string of filename
+  filename db "data.txt", 0 ; 0 terminated string of filename
 
 section .bss
-  filedata resb 100          ; reserve bytes for file data
+  text resb 100             ; reserve bytes for file data
 
 section .text
   global _start
 _start:
 
   ; Opens file and sets rax to file descriptor
-  mov rax, 3                ; sys_open
+  mov rax, 2                ; sys_open
   mov rdi, filename         ; set rdi to filename
   mov rsi, 0                ; set flag to readonly
   mov rdx, 0444o            ; set file permissions
@@ -27,14 +46,18 @@ _start:
   ; Reads file into filedata
   mov rdi, rax              ; move file descriptor to rdi
   mov rax, 0                ; sys_read
-  mov rsi, filedata         ; write to filedata
-  mov rdx, 100              ; amount of bytes to read
+  mov rsi, text             ; write to reserved bytes
+  mov rdx, 99               ; amount of bytes to read
   syscall                   ; call kernel
 
   ; Close file
   mov rax, 3                ; sys_close
   pop rdi                   ; set rdi to file descriptor
   syscall                   ; call kernel
+
+  ; Print file data
+  print text
   
+  ; Exit program
   exit
 
