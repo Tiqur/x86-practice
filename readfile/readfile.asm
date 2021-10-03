6f3c193e571cafcdf3e70b2dc4547542f8fdaec0
Tiqur <Tiqur7@gmail.com>
2021-10-02 17:43:31 -0700

    Read and close file

--- readfile/readfile.asm
+++ readfile/readfile.asm
@@ -7,10 +7,10 @@
 
 
 section .data
-  filename db "data.txt", 0 ; 0 terminated string of filename
+  filename db "data.txt", 0     ; 0 terminated string of filename
 
 section .bss
-  filedata resb 100         ; reserve bytes for file data
+  filedata resb 100          ; reserve bytes for file data
 
 section .text
   global _start
@@ -21,9 +21,20 @@ _start:
   mov rdi, filename         ; set rdi to filename
   mov rsi, 0                ; set flag to readonly
   mov rdx, 0444o            ; set file permissions
-  syscall
-  
+  syscall                   ; call kernel
+  push rax                  ; push file descriptor onto stack for future use
 
+  ; Reads file into filedata
+  mov rdi, rax              ; move file descriptor to rdi
+  mov rax, 0                ; sys_read
+  mov rsi, filedata         ; write to filedata
+  mov rdx, 100              ; amount of bytes to read
+  syscall                   ; call kernel
+
+  ; Close file
+  mov rax, 3                ; sys_close
+  pop rdi                   ; set rdi to file descriptor
+  syscall                   ; call kernel
   
   exit
 
